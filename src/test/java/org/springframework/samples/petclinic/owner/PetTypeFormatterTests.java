ff02b831d8caf6c8a155d8584abd20952f793e62
parkshihoo1119@gmail.com <shihoopark@Shihoos-MacBook-Pro.local>
2026-05-17 10:21:36 +1200

    fix: Remove unused imports (including wildcard imports)

--- src/test/java/org/springframework/samples/petclinic/owner/PetTypeFormatterTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTypeFormatterTests.java
@@ -21,7 +21,6 @@ import static org.mockito.BDDMockito.given;
 
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
 
